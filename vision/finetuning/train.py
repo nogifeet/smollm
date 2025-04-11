5fc075b984ecf22d8945af22e4b827b696beec2d
Nitin Kumar <72322393+nogifeet@users.noreply.github.com>
2025-04-11 07:08:50 +0530

    Update train.py

--- vision/finetuning/train.py
+++ vision/finetuning/train.py
@@ -1,6 +1,21 @@
+import argparse
+import multiprocessing
+import os
+
+import torch
+import transformers
+from accelerate import PartialState
+from datasets import load_dataset
+from peft import AutoPeftModelForCausalLM, LoraConfig
+from transformers import (
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+    is_torch_npu_available,
+    is_torch_xpu_available,
     logging,
     set_seed,
 )
+
 from trl import SFTTrainer
 
 
